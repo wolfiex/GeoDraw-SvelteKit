a75517b548d9355cda18319814f7310d07c333f7
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-06-20 11:55:56 +0100

    icons - still awaiting polygon

--- src/routes/draw/Toolbar/DrawButtons.svelte
+++ src/routes/draw/Toolbar/DrawButtons.svelte
@@ -34,8 +34,6 @@
       <Column>
         <!-- &nspar; -->
 
-
-      
         <Button
           id="move"
           on:click={buttonclick('move')}
@@ -43,7 +41,18 @@
           class="bx--btn bx--btn--secondary bx--btn--icon-only bx--tooltip__trigger bx--tooltip--a11y bx--tooltip--bottom bx--tooltip--align-center bx--btn--sm icon drawbtn"
         >
           <span class="bx--assistive-text">Move Map</span>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-cursor-move" width="24" height="24" viewBox="0 0 24 24"><path d="M13,6V11H18V7.75L22.25,12L18,16.25V13H13V18H16.25L12,22.25L7.75,18H11V13H6V16.25L1.75,12L6,7.75V11H11V6H7.75L12,1.75L16.25,6H13Z" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="mdi-cursor-move"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            ><path
+              d="M13,6V11H18V7.75L22.25,12L18,16.25V13H13V18H16.25L12,22.25L7.75,18H11V13H6V16.25L1.75,12L6,7.75V11H11V6H7.75L12,1.75L16.25,6H13Z"
+            /></svg
+          >
         </Button>
 
         <Button
@@ -54,7 +63,18 @@
           class="bx--btn bx--btn--secondary bx--btn--icon-only bx--tooltip__trigger bx--tooltip--a11y bx--tooltip--bottom bx--tooltip--align-center bx--btn--sm icon drawbtn"
         >
           <span class="bx--assistive-text">Draw Polygon</span>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-shape-polygon-plus" width="24" height="24" viewBox="0 0 24 24"><path d="M17,15.7V13H19V17L10,21L3,14L7,5H11V7H8.3L5.4,13.6L10.4,18.6L17,15.7M22,5V7H19V10H17V7H14V5H17V2H19V5H22Z" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="mdi-shape-polygon-plus"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            ><path
+              d="M17,15.7V13H19V17L10,21L3,14L7,5H11V7H8.3L5.4,13.6L10.4,18.6L17,15.7M22,5V7H19V10H17V7H14V5H17V2H19V5H22Z"
+            /></svg
+          >
         </Button>
 
         <Button
@@ -64,7 +84,9 @@
           class="bx--btn bx--btn--secondary bx--btn--icon-only bx--tooltip__trigger bx--tooltip--a11y bx--tooltip--bottom bx--tooltip--align-center bx--btn--sm icon drawbtn"
         >
           <span class="bx--assistive-text">Select a Radius</span>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-radius" width="24" height="24" viewBox="0 0 24 24"><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M17,15V13H13.72C13.36,13.62 12.71,14 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10C12.71,10 13.36,10.38 13.72,11H17V9L20,12L17,15Z" /></svg>
+          <svg width="24" height="24" viewBox='0 0 48 48 ' xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M24.0393 8c-2.211 0-4.2926.41978-6.2418 1.26088-1.9434.83862-3.6432 1.97802-5.0968 3.41832-1.4543 1.4409-2.6009 3.1348-3.43982 5.079C8.41997 19.707 8 21.7754 8 23.9607c0 2.211.41978 4.2926 1.26088 6.2418.83882 1.944 1.98512 3.644 3.43892 5.0977 1.4537 1.4538 3.1537 2.6001 5.0977 3.4389C19.7463 39.58 21.8147 40 24 40c2.211 0 4.2926-.4198 6.2418-1.2609 1.9442-.8389 3.6381-1.9855 5.079-3.4398 1.4403-1.4536 2.5797-3.1534 3.4183-5.0968C39.58 28.2537 40 26.1853 40 24c0-2.211-.4198-4.2926-1.2609-6.2418-.8387-1.9436-1.9783-3.6372-3.4192-5.0781s-3.1345-2.5805-5.0781-3.41922C28.293 8.41997 26.2246 8 24.0393 8Zm-9.286 6.7347c2.5521-2.5393 5.629-3.8061 9.2467-3.8061 3.6445 0 6.7274 1.2671 9.2658 3.8056 2.5385 2.5384 3.8056 5.6213 3.8056 9.2658 0 3.6177-1.2668 6.6946-3.8061 9.2467-2.5385 2.5514-5.6213 3.8247-9.2653 3.8247-3.6173 0-6.6939-1.273-9.2462-3.8252-2.5522-2.5523-3.8252-5.6289-3.8252-9.2462 0-3.644 1.2734-6.7268 3.8247-9.2653Z" />
+          </svg>
         </Button>
 
         <Button
@@ -74,7 +96,17 @@
           class="bx--btn bx--btn--secondary bx--btn--icon-only bx--tooltip__trigger bx--tooltip--a11y bx--tooltip--bottom bx--tooltip--align-center bx--btn--sm icon drawbtn"
         >
           <span class="bx--assistive-text">Click Select</span>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-cursor-default-click-outline" width="24" height="24" viewBox="0 0 24 24"><path d="M11.5,11L17.88,16.37L17,16.55L16.36,16.67C15.73,16.8 15.37,17.5 15.65,18.07L15.92,18.65L17.28,21.59L15.86,22.25L14.5,19.32L14.24,18.74C13.97,18.15 13.22,17.97 12.72,18.38L12.21,18.78L11.5,19.35V11M10.76,8.69A0.76,0.76 0 0,0 10,9.45V20.9C10,21.32 10.34,21.66 10.76,21.66C10.95,21.66 11.11,21.6 11.24,21.5L13.15,19.95L14.81,23.57C14.94,23.84 15.21,24 15.5,24C15.61,24 15.72,24 15.83,23.92L18.59,22.64C18.97,22.46 19.15,22 18.95,21.63L17.28,18L19.69,17.55C19.85,17.5 20,17.43 20.12,17.29C20.39,16.97 20.35,16.5 20,16.21L11.26,8.86L11.25,8.87C11.12,8.76 10.95,8.69 10.76,8.69M15,10V8H20V10H15M13.83,4.76L16.66,1.93L18.07,3.34L15.24,6.17L13.83,4.76M10,0H12V5H10V0M3.93,14.66L6.76,11.83L8.17,13.24L5.34,16.07L3.93,14.66M3.93,3.34L5.34,1.93L8.17,4.76L6.76,6.17L3.93,3.34M7,10H2V8H7V10" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="mdi-cursor-default-click-outline"
+            width="24"
+            height="24"
+            viewBox="0 0 50 50">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="m11.2137 11.2137 6.3993 21.3527 2.8391-8.5985 12.8484 12.8485 3.5159-3.5159-12.8485-12.8484 8.5985-2.8391-21.3527-6.3993Zm.1517-3.07769c-.4159-.13455-.8861-.18641-1.37406-.07624-.50105.11314-.91379.37309-1.23614.69543-.32234.32235-.58229.7351-.69543 1.23614-.11017.48796-.05831.95816.07624 1.37406l6.97569 23.2762c.1333.4761.3895.9648.8444 1.3385.4659.3827 1.0102.5378 1.5231.5612.5222.0237 1.0848-.088 1.5851-.4382.4918-.3443.787-.8237.9578-1.2934l.0076-.021 1.7407-5.2716 9.6883 9.6884c.4642.4642 1.0897.7945 1.8418.7945.7521 0 1.3776-.3303 1.8419-.7945l4.0631-4.0631c.4642-.4643.7945-1.0898.7945-1.8419s-.3303-1.3776-.7945-1.8418l-9.6884-9.6883 5.2716-1.7407.021-.0076c.4697-.1708.9491-.466 1.2934-.9578.3502-.5003.4619-1.0629.4382-1.5851-.0234-.5129-.1785-1.0572-.5612-1.5231-.3737-.4549-.8624-.7111-1.3385-.8444L11.3654 8.13601Z" />
+           </svg
+          >
         </Button>
       </Column>
     </Row>
--- src/routes/draw/Toolbar/EditButtons.svelte
+++ src/routes/draw/Toolbar/EditButtons.svelte
@@ -4,8 +4,8 @@
   import {writable, get} from 'svelte/store';
   import {onMount} from 'svelte/internal';
 
-  let disabled = 'false';
-  let fill = true;
+  export let disabled = 'false';
+  export let fill = true;
 
   //https://github.com/ramiroaisen/svelte-material-icons/search?q=click
   function init() {
@@ -35,43 +35,24 @@
             <svg
               width="24"
               height="24"
-              viewbox="0 0 48 48"
-              fill="whitesmoke"
+              viewBox="0 0 36 36"
               xmlns="http://www.w3.org/2000/svg"
-            > 
+            >
               <path
                 d="m34.7925 38.8834 4.3438-4.3438c.3187-.3187.8637-.093.8637.3577v4.3438c0 .2794-.2265.5059-.506.5059h-4.3437c-.4508 0-.6765-.5449-.3578-.8636ZM17.7889 27.2885c.4179 0 .7635-.1366 1.0368-.4098.2732-.2733.4099-.6189.4099-1.0368v-6.5581h6.6545c.3858 0 .7153-.1366.9886-.4099.2732-.2732.4098-.6188.4098-1.0367 0-.418-.1366-.7636-.4098-1.0368-.2733-.2733-.6189-.4099-1.0368-.4099h-6.6063V9.39842c0-.38577-.1367-.71528-.4099-.98854C18.5524 8.13663 18.2068 8 17.7889 8c-.4179 0-.7635.13663-1.0367.40988-.2733.27326-.4099.61884-.4099 1.03676v6.94386H9.39842c-.38577 0-.71528.1366-.98854.4099C8.13663 17.0736 8 17.4192 8 17.8372c0 .4179.13663.7635.40988 1.0367.27326.2733.61884.4099 1.03676.4099h6.89566v6.6063c0 .3858.1366.7153.4099.9886.2732.2732.6188.4098 1.0367.4098Z"
-                fill="#000"
+              
               />
             </svg>
           {:else}
             <span class="bx--assistive-text">Subtractive Selection</span>
             <svg
+              width="24"
+              height="24"
+              viewBox="0 0 36 36"
               xmlns="http://www.w3.org/2000/svg"
-              id="icon"
-              width="32"
-              height="32"
-              viewBox="0 0 32 32"
             >
-              <defs>
-                <style>
-                  .cls-1 {
-                    fill: none;
-                  }
-                </style>
-              </defs>
               <path
-                d="M12,10H22V4a2,2,0,0,0-2-2H4A2,2,0,0,0,2,4V20a2,2,0,0,0,2,2h6V12A2,2,0,0,1,12,10Z"
-              />
-              <path
-                d="M28,10H22V20a2,2,0,0,1-2,2H10v6a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V12A2,2,0,0,0,28,10Z"
-              />
-              <rect
-                id="_Transparent_Rectangle_"
-                data-name="&lt;Transparent Rectangle&gt;"
-                class="cls-1"
-                width="32"
-                height="32"
+                d="m34.7925 34.7884 4.3438-4.3438c.3187-.3187.8637-.093.8637.3578v4.3437c0 .2795-.2265.506-.506.506h-4.3437c-.4508 0-.6765-.545-.3578-.8637ZM9.44664 14.8933H25.8901c.3858 0 .7153-.1447.9885-.434.2733-.2893.4099-.6269.4099-1.0127 0-.4179-.1366-.7635-.4099-1.0367-.2732-.2733-.6188-.4099-1.0367-.4099H9.39842c-.38577 0-.71528.1447-.98854.434C8.13663 12.7233 8 13.0609 8 13.4466c0 .418.13663.7635.40988 1.0368.27326.2733.61884.4099 1.03676.4099Z"
               />
             </svg>
           {/if}
@@ -89,7 +70,18 @@
           class="bx--btn bx--btn--secondary bx--btn--icon-only bx--tooltip__trigger bx--tooltip--a11y bx--tooltip--bottom bx--tooltip--align-center bx--btn--sm icon"
         >
           <span class="bx--assistive-text">Undo</span>
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-undo-variant" width="24" height="24" viewBox="0 0 24 24"><path d="M13.5,7A6.5,6.5 0 0,1 20,13.5A6.5,6.5 0 0,1 13.5,20H10V18H13.5C16,18 18,16 18,13.5C18,11 16,9 13.5,9H7.83L10.91,12.09L9.5,13.5L4,8L9.5,2.5L10.92,3.91L7.83,7H13.5M6,18H8V20H6V18Z" /></svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="mdi-undo-variant"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            ><path
+              d="M13.5,7A6.5,6.5 0 0,1 20,13.5A6.5,6.5 0 0,1 13.5,20H10V18H13.5C16,18 18,16 18,13.5C18,11 16,9 13.5,9H7.83L10.91,12.09L9.5,13.5L4,8L9.5,2.5L10.92,3.91L7.83,7H13.5M6,18H8V20H6V18Z"
+            /></svg
+          >
         </Button>
       </Column>
     </Row>
@@ -100,4 +92,7 @@
 </main>
 
 <style>
+  svg path {
+    fill: rgb(255, 255, 255);
+  }
 </style>
